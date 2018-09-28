de6effa5fb69da22be77b83635b9da6f52cfd5f4
PaservanYu <paservanyu@kkbox.com>
2018-09-28 19:44:31 +0800

    fix: take screenshot when StringException is occurred

--- common/exceptions/StringException.java
+++ common/exceptions/StringException.java
@@ -1,8 +1,12 @@
 package com.kddi.android.UtaPass.sqa_espresso.common.exceptions ;
 
+import com.kddi.android.UtaPass.sqa_espresso.common.UtaPassUtil;
+
 public class StringException extends RuntimeException {
     public StringException( String msg ) {
         super( msg ) ;
+
+        UtaPassUtil.takeScreenshot( this.getClass().getSimpleName() ) ;
     }
 }
 

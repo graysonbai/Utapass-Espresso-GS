b7630a6571d47388e635babea9af4ef85ef268d4
PaservanYu <paservanyu@kkbox.com>
2018-10-05 17:03:04 +0800

    fix: inappropriate matcher.

--- pages/stream/_lineup/WhatsNewLineUp.java
+++ pages/stream/_lineup/WhatsNewLineUp.java
@@ -19,7 +19,7 @@ public class WhatsNewLineUp extends LineUpObject {
 
 
     protected Matcher<View> getMatcherToCountMaxIndexOfWindow() {
-        return withId( R.id.item_three_cover_playlist_cover_1 ) ;
+        return withId( R.id.item_three_cover_playlist_root_layout ) ;
     }
 
     protected String getTitleOfLineUpInEnglish() {
@@ -35,20 +35,23 @@ public class WhatsNewLineUp extends LineUpObject {
 
         CardObject card = new CardObject() ;
 
-        card.playButton( () -> UtaPassUtil.withIndex(
-                allOf( withId( R.id.item_three_cover_playlist_play ),
-                       isDescendantOfA( this.getMatcherToFindRecycleView() ) ),
-                indexInWindow ) ) ;
+        card.playButton( () -> allOf(
+                withId( R.id.view_playlist_play_icon ),
+                isDescendantOfA( UtaPassUtil.withIndex(
+                        this.getMatcherToCountMaxIndexOfWindow(),
+                        indexInWindow ) ) ) ) ;
 
-        card.title( () -> UtaPassUtil.withIndex(
-                allOf( withId( R.id.item_three_cover_playlist_title ),
-                       isDescendantOfA( this.getMatcherToFindRecycleView() ) ),
-                indexInWindow ) ) ;
+        card.title( () -> allOf(
+                withId( R.id.item_three_cover_playlist_title ),
+                isDescendantOfA( UtaPassUtil.withIndex(
+                        this.getMatcherToCountMaxIndexOfWindow(),
+                        indexInWindow ) ) ) ) ;
 
-        card.likedCount( () -> UtaPassUtil.withIndex(
-                allOf( withId( R.id.item_three_cover_playlist_like_count ),
-                       isDescendantOfA( this.getMatcherToFindRecycleView() ) ),
-                indexInWindow ) ) ;
+        card.likedCount( () -> allOf(
+                withId( R.id.item_three_cover_playlist_like_count ),
+                isDescendantOfA( UtaPassUtil.withIndex(
+                        this.getMatcherToCountMaxIndexOfWindow(),
+                        indexInWindow ) ) ) ) ;
 
         return card ;
     }

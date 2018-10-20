3a6b98a168b32ccdd636b1e842b73e2af10ec5f9
PaservanYu <paservanyu@kkbox.com>
2018-10-21 01:03:57 +0800

    fix: only [a-zA-Z0-9_-] is supported for screenshot tag
    pass original message to dprint()

--- common/exceptions/StringNotInException.java
+++ common/exceptions/StringNotInException.java
@@ -1,10 +1,15 @@
 package com.kddi.android.UtaPass.sqa_espresso.common.exceptions ;
 
+import com.kddi.android.UtaPass.sqa_espresso.common.UtaPassUtil;
+
 public class StringNotInException extends BasicException {
 
-    public StringNotInException( String actual, String[] sets ) {
-        super( String.format( "actual = '%s', Sets = '%s'",
-                              actual,
-                              String.join(",", sets ) ) ) ;
+    public StringNotInException( String label, String actual, String[] sets ) {
+        super( label ) ;
+
+        UtaPassUtil.dprint( String.format(
+                "actual = '%s', Sets = '%s'",
+                actual,
+                String.join(",", sets ) ) ) ;
     }
 }

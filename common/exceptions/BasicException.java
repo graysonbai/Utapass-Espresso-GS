72285d0d55728c9b74624ddc0c7398814dd11cfe
PaservanYu <paservanyu@kkbox.com>
2018-12-05 18:19:42 +0800

    fix: only [a-zA-Z0-9_-] is supported for screenshot tag

--- common/exceptions/BasicException.java
+++ common/exceptions/BasicException.java
@@ -13,6 +13,7 @@ public class BasicException extends RuntimeException {
                      .replace( "(", "_" )
                      .replace( ")", "_" )
                      .replace( "=", "_" )
+                     .replace( ",", "_" )
                      .replace( "'", "" )
         ) ;
     }
